36d21b9084d178a71a72c5a5f157aec3059de7b8
cnexcale <lukas.r@freenet.de>
2022-03-13 10:52:24 +0100

    added Http POST handler and dummy request body builder to IgMonitor

--- plugins/nf-ignite/src/main/nextflow/util/IgMonitor.groovy
+++ plugins/nf-ignite/src/main/nextflow/util/IgMonitor.groovy
@@ -1,11 +1,15 @@
 package nextflow.util
 
 import groovy.json.JsonBuilder
+import groovy.util.logging.Slf4j
+
+import java.net.*
 import groovy.json.JsonSlurper
 import nextflow.Session
 import nextflow.executor.IgBaseTask
 import nextflow.scheduler.Protocol.Resources
 
+@Slf4j
 class IgMonitor {
     private static String MONITORING_ENDPOINT
 
@@ -14,13 +18,49 @@ class IgMonitor {
     private JsonBuilder json
 
     IgMonitor(Map config) {
-        this.monitorApi = new URL(config.get(MONITORING_ENDPOINT))
+        // TODO - setup Ignite config section
+        this.monitorApi = new URL((String)config.get(MONITORING_ENDPOINT))
 
     }
-    void sendUpdate(IgMonitoringUpdate update) {
-        def payload = new JsonBuilder(update).toString()
-        def post = monitorApi.openConnection()
-        post.set
 
+    IgMonitor(String monitoringEndpoint) {
+        this.monitorApi = new URL(monitoringEndpoint)
+    }
+
+    void sendUpdate(IgMonitoringUpdate update) {
+        log?.debug("Sending monitoring update for task ${update.task.taskId} from node ${update.nodeId}")
+
+        def payload = new JsonBuilder(buildRequest(update))
+                                .toString()
+        try {
+            post(payload)
+        } catch (Exception e) {
+            log?.error("Monitoring update failed", e)
+        }
+
+    }
+
+    private void post(String body) {
+        HttpURLConnection post = monitorApi.openConnection()
+        post.setRequestMethod("POST")
+        post.setRequestProperty("accept", "application/json")
+        post.setDoOutput(true)
+
+        post.with {
+            it.outputStream.withWriter { outputStreamWriter ->
+                outputStreamWriter << body
+            }
+        }
+
+        if (post.responseCode == 200) {
+            log?.debug("post successful")
+        } else {
+            log?.error("could not send POST to ${post?.getURL()?.toString()}: ${post.responseMessage}")
+        }
+    }
+
+    private String buildRequest(IgMonitoringUpdate update) {
+        // TODO - create proper api request object
+        return ""
     }
 }
