08a14202fb462277b9d6b32e38ea534ac8350b79
cnexcale <36278502+cnexcale@users.noreply.github.com>
2022-03-15 23:21:20 +0100

    updated ig-monitoring update object

--- plugins/nf-ignite/src/main/nextflow/util/IgMonitoringUpdate.groovy
+++ plugins/nf-ignite/src/main/nextflow/util/IgMonitoringUpdate.groovy
@@ -1,14 +1,54 @@
 package nextflow.util
 
-import nextflow.Session
 import nextflow.executor.IgBaseTask
 import nextflow.scheduler.Protocol.Resources
 
 class IgMonitoringUpdate {
-    IgBaseTask task
-    Resources nodeResources
-    Session session
     String nodeId
-    int queueSize
+    String clusterGroup
+    IgBaseTask task
+    String event
+    Resources nodeResources
+    Resources currentResources
     int activeTasks
+    int pendingTasks
+    Throwable error
+}
+
+class IgMonitoringUpdateDto {
+
+    class ResourceInfo {
+        Resources total
+        Resources current
+
+        ResourceInfo(Resources total, Resources current) {
+            this.total = total
+            this.current = current
+        }
+    }
+
+    String nodeId
+    String clusterGroup
+    String taskName
+    String event
+    ResourceInfo resources
+    int pendingTasks
+    int activeTasks
+    String error
+
+
+    IgMonitoringUpdateDto() {
+        // for unit testing purpose only
+    }
+
+    IgMonitoringUpdateDto(IgMonitoringUpdate update) {
+        nodeId = update.nodeId
+        clusterGroup = update.clusterGroup
+        event = update.event
+        taskName = update.task.taskName
+        pendingTasks = update.pendingTasks
+        activeTasks = update.activeTasks
+        error = update.error?.toString()
+        resources = new ResourceInfo(update.nodeResources, update.currentResources)
+    }
 }
